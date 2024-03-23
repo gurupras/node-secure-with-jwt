42a2e3d7f70b6cf1cff302c1e2fb971193c37545
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2024-03-23 13:37:47 -0400

    Remove ssr

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     sourcemap: true,
-    ssr: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'index.js'),
